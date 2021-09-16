72c7aaa1e2b1b8033561bb3d3196249a16cafc3e
Prakhar <prakharpks02@gmail.com>
2021-09-16 18:36:20 +0530

    gateway working in mobile view, documentation and options button working

--- js/gateway-demo.js
+++ js/gateway-demo.js
@@ -1,15 +1,88 @@
+var vendorListVar = [{
+        "name": "Vega Delivery",
+        "id": "vega",
+        "eta": "6th October, 2021",
+        "cost": "50"
+    },
+    {
+        "name": "ShadowFax",
+        "id": "shadowfax",
+        "eta": "7th October, 2021",
+        "cost": "40"
+    },
+    {
+        "name": "Wefast",
+        "id": "wefast",
+        "eta": "6th October, 2021",
+        "cost": "70"
+    },
+    {
+        "name": "Dunzo",
+        "id": "dunzo",
+        "eta": "6th October, 2021",
+        "cost": "80"
+    },
+]
+
 var app1 = new Vue({
     el: '#main-page-container-id',
     delimiters: ['[[', ']]'],
     data: {
-        'pageStatus':'gateway',
+        'pageStatus': 'cart',
+        'vendorList': vendorListVar,
+        'allVendorList': vendorListVar,
+        'selectedVendor': {
+            'name': '',
+            'id': '',
+            'cost': 0,
+            'eta': '',
+        },
     },
     methods: {
         CheckoutButton: function () {
             this.pageStatus = 'gateway';
+            this.selectedVendor = {
+                'name': '',
+                'id': '',
+                'cost': 0,
+                'eta': '',
+            };
         },
-    },
-    mounted() {
-        
+        RadioClick: function (vendorid, vendorname, vendoreta, vendorcost, event) {
+
+            let allVendorContainers = document.getElementsByClassName('vendor-container');
+
+            for (let i = 0; i < allVendorContainers.length; i++) {
+                allVendorContainers[i].classList.remove('vendor-container-active');
+            };
+
+            let targetElement = event.target.parentElement.parentElement.parentElement;
+            targetElement.classList.add('vendor-container-active');
+
+            console.log(vendorid);
+
+            this.selectedVendor.id = vendorid;
+            this.selectedVendor.name = vendorname;
+            this.selectedVendor.eta = vendoreta;
+            this.selectedVendor.cost = parseFloat(vendorcost);
+        },
+        ConfirmDelivery: function () {
+            this.pageStatus = 'success';
+        },
+        OpenDocumentation: function () {
+            document.getElementById("document-container-id").style.display="block";
+            document.getElementById("options-container-id").style.display="none";
+            document.getElementById("gateway-container-id").style.display="none";
+        },
+        OpenOptions: function () {
+            document.getElementById("document-container-id").style.display="none";
+            document.getElementById("options-container-id").style.display="block";
+            document.getElementById("gateway-container-id").style.display="none";
+        },
+        OpenGateway: function () {
+            document.getElementById("document-container-id").style.display="none";
+            document.getElementById("options-container-id").style.display="none";
+            document.getElementById("gateway-container-id").style.display="block";
+        },
     },
 });
